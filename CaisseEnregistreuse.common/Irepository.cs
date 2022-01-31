b3aadf1bc6c840e11a96d6fd3172cf55e57da7e6
arolleaguekeng <arolleaguekeng@gmail.com>
2022-01-31 19:25:20 +0100

    ajout de l'interface Irepository  et de ces méthodes

---
+++ CaisseEnregistreuse.common/Irepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaisseEnregistreuse.common
+{
+    interface Irepository<T>
+    {
+        T Add(T t);
+        T Get(T t);
+        IEnumerable<T> Find(T t);
+    }
+}
