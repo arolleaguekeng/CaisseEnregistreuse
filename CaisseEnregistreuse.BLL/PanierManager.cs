cafc89944cb0a484bc996170ddc23c08e828d40b
Florient Manfo <manfoflorient@outlook.fr>
2022-02-03 15:34:29 +0100

    Création des classe PanierDAO, AchatDAO, ProduitDAO

--- CaisseEnregistreuse.BLL/PanierManager.cs
+++ CaisseEnregistreuse.BLL/PanierManager.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿
+using CaisseEnregistreuse.BO;
+using CaisseEnregistreuse.DAL;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CaisseEnregistreuse.BLL
 {
     public class PanierManager
     {
+        private PanierDAO panierDAO;
+
+        public PanierManager()
+        {
+            panierDAO = new PanierDAO();
+        }
+
+        public Panier Add(Panier panier)
+        {
+            return panierDAO.Add(panier);
+        }
+
+        public Panier Get(Panier panier)
+        {
+            return panierDAO.Get(panier);
+        }
+
+        public List<Panier> Find(Panier p)
+        {
+            return panierDAO.Find(p).ToList();
+        }
     }
 }
